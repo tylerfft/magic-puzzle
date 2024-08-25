f3578ad19afedf48fb3bc4590e82e1694af55b45
tyler <12198276+tyler-fft@users.noreply.github.com>
2024-08-25 15:54:24 +0800

    fix typo

--- src/components/Puzzel/Challenge/index.jsx
+++ src/components/Puzzel/Challenge/index.jsx
@@ -3,5 +3,5 @@ import React from "react";
 import styles from "./index.module.css";
 
 export default function Challenge() {
-  return <div className={styles.welcome}>Challenge Magic Puzzel</div>;
+  return <div className={styles.welcome}>Challenge Magic Puzzle</div>;
 }
